dddea08810d3492e12afb26ffe508ac57028d117
hollyroisum <hollyroisum@u.boisestate.edu>
2019-04-04 16:23:58 -0600

    DAO changes

--- Dao.php
+++ Dao.php
@@ -1,13 +1,19 @@
 <?php
+
 class Dao {
     private $host = "us-cdbr-iron-east-03.cleardb.net";
     private $db = "heroku_4e9d86f67afd238";
     private $user = "bd63a5978ac525";
     private $pass = "084510d5";
-
+    
     public function getConnection () {
-    return
-    new PDO("mysql:host={$this->host};dbname={$this->db}")
+        try {
+          $conn = new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user,
+                $this->pass);
+        } catch (Exception $e) {
+          echo print_r($e,1);
+          exit;
+        }
+        return $conn;
     }
-
 }
